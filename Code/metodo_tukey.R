53dcdc2538d8b8bdc6b1a7d73dc05eafc02cce66
YanMoreno <yeanguerra14@gmail.com>
2025-04-21 22:29:29 -0500

    Correcion funcion ejemplo tukey, estimacion df1  y s1

--- Code/metodo_tukey.R
+++ Code/metodo_tukey.R
@@ -28,7 +28,7 @@
 #'
 #' @examples
 #' set.seed(123)
-#' vector_ <- rnorm(1000, mean = 30, sd = 6)
+#' vector_ <- rnorm(1000, mean = 948.6833, sd = 30)
 #' calcular_S1_df1(vector_interes = vector_, Si = 0.07, Ss = 0.12)
 #'
 #' @export
