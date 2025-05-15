a37c6873acac1e8967bdc6fae303b7b035c6d686
Diana Hernández <dianacatalinahernandezrojas@gmail.com>
2025-05-15 11:19:21 -0500

    Funciones de costos sin ejemplos ni bullets que se debian eliminar, ya se encuentran los ejemplos en el rmd.

--- Front_APP_DEXP/ui.R
+++ Front_APP_DEXP/ui.R
@@ -75,14 +75,7 @@ ui <-tagList(
                   width = 12,
                   h3("Asignación de réplicas por variabilidad"),
                   p("Esta herramienta calcula cuántas réplicas son necesarias para cada tratamiento en un diseño experimental, sin tener en cuenta los costos, pero equilibrando la precisión de los tratamientos según su variabilidad. Los tratamientos con mayor variabilidad recibirán más réplicas."),
-                  tags$ul(
-                    tags$li("Cuando los costos no son una preocupación, pero se desea equilibrar la precisión, la herramienta distribuye las réplicas de forma proporcional a la variabilidad de cada tratamiento. Esto significa que los tratamientos más variables recibirán más réplicas para mejorar la precisión de los resultados."),
-                    tags$li("La herramienta toma como entrada el número de tratamientos, el número total de réplicas iniciales y la desviación estándar de cada tratamiento para distribuirlas de manera eficiente.")
-                  ),
-                  br(),
-                  p(strong("Ejemplo de aplicación:")),
-                  p("Supongamos que tienes cuatro tratamientos con diferentes desviaciones estándar dadas por (6.27, 9.57, 12, 3.32) y un número total de réplicas iniciales de 5. La herramienta calculará cuántas réplicas deben asignarse a cada tratamiento, distribuyendo las réplicas de manera proporcional a la variabilidad de cada tratamiento. Los tratamientos con mayor desviación estándar recibirán más réplicas para mejorar la precisión de los resultados."),
-                  p("Para mayor información accede a:", a("hola", href = "https://escuelaing.s3.amazonaws.com/production/documents/Programación_Académica_Pregrado_Periodo_2025-i.pdf?AWSAccessKeyId=AKIAWFY3NGTFJHVI634A&Signature=FNQU9BVTAGB1mt0WKOCEy2BHUMA%3D&Expires=1748480035"))
+                  p("Para mayor información accede a:", a("Info dexp app", href = "https://escuelaing.s3.amazonaws.com/production/documents/Programación_Académica_Pregrado_Periodo_2025-i.pdf?AWSAccessKeyId=AKIAWFY3NGTFJHVI634A&Signature=FNQU9BVTAGB1mt0WKOCEy2BHUMA%3D&Expires=1748480035"))
                 )
               )
               
@@ -167,15 +160,7 @@ ui <-tagList(
                   width = 12,
                   h3("Asignación de réplicas con restricción presupuestaria"),
                   p("Esta herramienta calcula cuántas réplicas son necesarias para cada tratamiento en un diseño experimental, considerando los costos por tratamiento y el presupuesto para llevarlo a cabo, con el objetivo de optimizar la precisión de los resultados dentro de un presupuesto limitado."),
-                  tags$ul(
-                    tags$li("Cuando el presupuesto es una restricción y los tratamientos tienen diferentes costos y niveles de variabilidad, la herramienta distribuye las réplicas de manera eficiente utilizando un enfoque basado en multiplicadores de Lagrange."),
-                    tags$li("Esto permite asignar más réplicas a los tratamientos más variables teniendo en cuenta el presupuesto total disponible, maximizando así la precisión del diseño experimental."),
-                    tags$li("La herramienta toma como entrada el número de tratamientos, la desviación estándar de cada tratamiento, el costo por unidad experimental de cada tratamiento y el presupuesto total disponible.")
-                  ),
-                  br(),
-                  p(strong("Ejemplo de aplicación:")),
-                  p("Supongamos que tienes cuatro tratamientos con desviaciones estándar dadas por (6.27, 9.57, 12, 3.32), los costos por tratamiento son de (1000, 200, 700, 1100) y un presupuesto total de $50.000. La herramienta calculará cuántas réplicas deben asignarse a cada tratamiento, distribuyendo los recursos disponibles de forma que se mejore la precisión teniendo en cuenta el presupuesto disponible."),
-                  p("Para mayor información accede a:", a("hola", href = "https://escuelaing.s3.amazonaws.com/production/documents/Programación_Académica_Pregrado_Periodo_2025-i.pdf?AWSAccessKeyId=AKIAWFY3NGTFJHVI634A&Signature=FNQU9BVTAGB1mt0WKOCEy2BHUMA%3D&Expires=1748480035"))
+                  p("Para mayor información accede a:", a("Info app dexp", href = "https://escuelaing.s3.amazonaws.com/production/documents/Programación_Académica_Pregrado_Periodo_2025-i.pdf?AWSAccessKeyId=AKIAWFY3NGTFJHVI634A&Signature=FNQU9BVTAGB1mt0WKOCEy2BHUMA%3D&Expires=1748480035"))
                 )
               )
               
@@ -265,11 +250,11 @@ ui <-tagList(
                   width = 12,
                   h3("Asignación de tratamientos y réplicas con Función de Costos y Varianza Máxima"),
                   p("La asignación de tratamientos y réplicas en un diseño experimental se basa en un modelo de componentes de varianza, donde tanto el número de tratamientos como el número de réplicas son variables. Estos valores se ajustan de acuerdo con la necesidad de controlar las varianzas y minimizar los costos en la estimación de la media de los tratamientos. La varianza de la media muestral es una medida clave en este proceso y está determinada por las varianzas asociadas a los tratamientos y las réplicas. El desafío es encontrar los valores óptimos de tratamientos y réplicas que minimicen una función de costos dada, que incluye tanto el costo por unidad de tratamiento como el costo por unidad experimental. Este proceso matemático, descrito por Mendenhall (1968), busca la distribución eficiente de los recursos experimentales, ajustando el número de tratamientos y réplicas de manera que se mantenga constante la varianza de la media muestral, maximizando así la precisión del diseño teniendo en cuenta el presupuesto disponible."),
-                  br(),
-                  p(strong("Ejemplo de aplicación:")),
-                  p("En este ejemplo, se busca determinar el número óptimo de toros y terneros necesarios para un experimento genético, considerando restricciones de costo y precisión. La varianza máxima permitida para la media muestral es de 43,49, lo que establece un límite sobre la variabilidad aceptable en los resultados. Además, se tienen en cuenta los costos experimentales, siendo de $150.000 por cada toro y $50.000 por cada ternero. A partir de estos valores y las estimaciones de varianza obtenidas en el ejemplo anterior (como la varianza entre toros y el error experimental), se concluye que para cumplir con las condiciones establecidas se deben seleccionar 7 toros y 3 terneros por cada toro. Este diseño permite mantener el equilibrio entre el costo total del experimento y la precisión estadística deseada.")
+                  p("Para mayor información accede a:", a("Info app dexp", href = "https://escuelaing.s3.amazonaws.com/production/documents/Programación_Académica_Pregrado_Periodo_2025-i.pdf?AWSAccessKeyId=AKIAWFY3NGTFJHVI634A&Signature=FNQU9BVTAGB1mt0WKOCEy2BHUMA%3D&Expires=1748480035"))
+                  
+           
                 ),
-                p("Para mayor información accede a:", a("hola", href = "https://escuelaing.s3.amazonaws.com/production/documents/Programación_Académica_Pregrado_Periodo_2025-i.pdf?AWSAccessKeyId=AKIAWFY3NGTFJHVI634A&Signature=FNQU9BVTAGB1mt0WKOCEy2BHUMA%3D&Expires=1748480035"))
+                
 
                 
               ),
